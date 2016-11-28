0b85bb434b5298239d0b22e14bff1b5ba880aebb
Mason Braun <mason.braun@taxi.ca>
2016-11-28 08:14:37 -0500

    move string replace into its own function

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,14 @@
 import React, { PropTypes } from 'react'
 
+const clean = (text) => {
+  let newtext = text.replace(/\n/g, " - ");
+  return newtext;
+}
+
 const Movie = ({ onClick, title, year, actors }) => (
   <div className="movie">
     <p className="movie__details movie__details--title">{title} ({year})</p>
-    <p className="movie__details movie__details--actors">Starring: {actors.replace(/\n/g, " - ")}</p>
+    <p className="movie__details movie__details--actors">Starring: {clean(actors)}</p>
     <span className="movie__delete" onClick={onClick}>X</span>
   </div>
 )
